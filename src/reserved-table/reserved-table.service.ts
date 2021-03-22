6f82f5564051d4646f821e78dcbb8e5dbc6a6cc5
Emiliejean <jean.emilie34@hotmail.fr>
2021-03-22 22:50:53 +0100

    reserved table service

--- src/reserved-table/reserved-table.service.ts
+++ src/reserved-table/reserved-table.service.ts
@@ -1,26 +1,56 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException, BadRequestException } from '@nestjs/common';
 import { CreateReservedTableDto } from './dto/create-reserved-table.dto';
 import { UpdateReservedTableDto } from './dto/update-reserved-table.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { ReservedTable } from './entities/reserved-table.entity';
+import { Repository } from 'typeorm';
+import { Reservation } from 'src/reservation/entities/reservation.entity';
+import { Price } from 'src/price/entities/price.entity';
 
 @Injectable()
 export class ReservedTableService {
-  create(createReservedTableDto: CreateReservedTableDto) {
-    return 'This action adds a new reservedTable';
+  constructor(
+    @InjectRepository(ReservedTable) private reservedtableRepository: Repository<ReservedTable>,
+    @InjectRepository(Price) private priceRepository: Repository<Price>,
+    @InjectRepository(Reservation) private reservationRepository: Repository<Reservation>
+    ) {}
+
+  async create(createReservedTableDto: CreateReservedTableDto) {
+    const { priceId, reservationId, ...dto } = createReservedTableDto;
+    const price = await this.priceRepository.findOne(priceId);
+    const reservation = await this.reservationRepository.findOne(reservationId);
+    if (price){
+      if (reservation){
+        return this.reservedtableRepository.save({price, reservation, ...dto});
+      }
+      else {
+        throw new BadRequestException();
+      }
+    }
+    else {
+        throw new BadRequestException();
+      }
   }
 
   findAll() {
-    return `This action returns all reservedTable`;
+    return this.reservedtableRepository.find();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} reservedTable`;
+  async findOne(id: number) {
+    const reservedTable = await this.reservedtableRepository.findOne(id);
+    if (reservedTable) {
+      return reservedTable;
+    }
+    else {
+      throw new NotFoundException(`No reserved table found with id ${id}`);
+    }
   }
 
   update(id: number, updateReservedTableDto: UpdateReservedTableDto) {
-    return `This action updates a #${id} reservedTable`;
+    return this.reservedtableRepository.update(id, updateReservedTableDto);
   }
 
   remove(id: number) {
-    return `This action removes a #${id} reservedTable`;
+    return this.reservedtableRepository.delete(id);
   }
 }
