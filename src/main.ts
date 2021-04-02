9dc9b146bb44b261e435a841cc944f397b069e18
Keven <keven.dvorianoff@etu.umontpellier.fr>
2021-04-02 17:16:02 +0200

    Create default admin/admin account if no account exists

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,11 @@
 import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 import { AppModule } from './app.module';
+import { User } from './user/entities/user.entity';
+import { UserService } from './user/user.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -12,6 +16,17 @@ async function bootstrap() {
   }));
 
   const config = app.get(ConfigService);
+
+  const userRepository: Repository<Account> = app.get(getRepositoryToken(User));
+  const userService = app.get(UserService);
+  const userCounts = await userRepository.count();
+  if (userCounts === 0) {
+    await userService.create({
+      isAdmin: true,
+      username: 'admin',
+      password: 'admin'
+    });
+  }
 
   app.enableCors();
   await app.listen(config.get('PORT') || 3000);
